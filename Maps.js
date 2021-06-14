9695a059f58a9ac9daac39c473acb0adf8d75a98
dsdkv9 <dsdkv9@gmail.com>
2021-06-14 10:19:04 +0530

    Merged map code

--- Maps.js
+++ Maps.js
@@ -18,6 +18,7 @@ class NationalId{
 
 let citizen1 = new NationalId(101, 'name1', 'male', '10/10/1980', 'abc1', 'xyz1');
 let citizen2 = new NationalId(102, 'name2', 'female', '10/10/2000', 'abc2', 'xyz2');
+
 console.log("citizen1: " + citizen1);
 
 let empMap = new Map([
@@ -26,15 +27,46 @@ let empMap = new Map([
     [1003, 'Emp 3'],
     [1004, 'Emp 4'],
     [1005, citizen1],
-    [1005, citizen2],
+    [1006, citizen2],
 ]);
 
-console.log("empMap: ");
-console.log(empMap);
-console.log("empMap.get(1005): ")
-console.log(empMap.get(1005));
+// console.log("empMap: ");
+// console.log(empMap);
+// console.log("empMap.get(1005): ")
+// console.log(empMap.get(1005));
 // console.info("empMap.keys(): ");
 // console.info(empMap.keys());
 // console.log("empMap.values(): ");
 // console.log(empMap.values());
 
+// let empArrayList = [[1001, 'Emp 1'], [1002, 'Emp 2'], [1003, 'Emp 3']];
+
+ //let empMap
+
+
+ let studentsMap = new Map();
+ studentsMap.set('S0001', 'Student 1');
+ studentsMap.set('S0002', 'Student 2');
+ studentsMap.set('S0003', 'Student 1');
+
+ console.log(studentsMap);
+
+ console.log(studentsMap.has(1001));
+ console.log(studentsMap.has('S0001'));
+
+ let recipeMap = new Map([
+    ['cucumber', 500],
+    ['tomatoes', 350],
+    ['onion',    50]
+  ]);
+
+  for (let entry of recipeMap) { // the same as of recipeMap.entries()
+    console.log(entry); // cucumber,500 (and so on)
+  }
+ 
+
+  recipeMap.forEach((v, k, m) => {
+    console.log(k);
+    console.log(v);
+    console.log(m);
+  });
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Javascript sample for codes for Test Automation Engineers. 
+
+object-oriented-programming-in-javascript: 
+-   https://betterprogramming.pub/object-oriented-programming-in-javascript-b3bda28d3e81
+
+-   https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
+
+-   
+
