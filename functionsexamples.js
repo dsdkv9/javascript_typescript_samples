66290a6d196b3f1286d3a8f83a07ad728d969074
dsdkv9 <dsdkv9@gmail.com>
2021-06-16 12:00:00 +0530

    function examples

--- functionsexamples.js
+++ functionsexamples.js
@@ -1,10 +1,44 @@
-(function () {
-    console.log("anonymous function");
-})();
+// a = 10;
+// b = 20;
+// c = 11;
+// x = calculation(a, b, c);
+// console.log(x);
 
-function sum(a = 10, b = 20) {
-    console.log("Normal function");
+// y = a * b;
+
+// z = calculation(10, 2, 4);
+// console.log(z);
+
+// function calculation(i, j, k) {
+//     return i * i + j * j + k * k;
+// }
+
+// Math.sqrt();
+
+// console.log('****** Anonymous Function ******');
+// (function () {
+//     console.log("anonymous function");
+// })();
+
+console.log('****** Basic Function Declaration Example ******');
+
+console.log(sum(90, 21));
+
+function sum(a, b = 2) {
+    console.log(`a = ${a} & b = ${b}`);
     return a + b;
 }
 
-console.log(sum(30));
+console.log(sum(10, 5));
+
+let tempSumFuncVar = sum;
+console.log(tempSumFuncVar(21, 1.1));
+
+console.log('****** Decalare Function as Variable Example ******');
+
+let multiFuncVar = æ(x, y) {
+    console.log(`x = ${x} & y = ${y}`);
+    return x * y;
+}
+
+console.log(multiFuncVar(2, 6));
