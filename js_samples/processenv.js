1f2eb9e79ce629ecbe56b6e3637a585ac066b077
Deepak verma <dsdkv9@gmail.com>
2021-07-12 18:35:27 +0530

    written code to get value from object

--- js_samples/processenv.js
+++ js_samples/processenv.js
@@ -4,18 +4,67 @@ const yaml = require('js-yaml');
 
 try {
     let fileContents = fs.readFileSync('./configyaml.yaml', 'utf8');
-    let data = yaml.load(fileContents);
-    console.log(typeof data);
-    
-    let objAttrList = Object.getOwnPropertyNames(data);
+    let configurationsObj = yaml.load(fileContents);
+    console.log(typeof configurationsObj);
 
-    console.log(`Object attributes list: ${objAttrList}`);
-    for(let attr of objAttrList) {
-        let value = data[attr];
-        console.log(`Object attribute ${attr} and value:`);
-        console.log(value);
-        console.log(value["url"]);
+    // console.log('****** Without Recurssion - Start******');
+    // Object.keys(configurationsObj).forEach(function (key) {
+    //     //let value = configurationsObj[key];
+    //     //console.log(typeof key);
+    //     console.log(key);
+    //     console.log(typeof configurationsObj[key]);
+    //     console.log(configurationsObj[key]);
+    // });
+    // console.log('****** Without Recurssion - End******\n');
+
+    console.log('****** With Recurssion - Start******');
+    function getKey(obj, res = [], parent = '') {
+        const keys = Object.keys(obj);
+
+        /** Loop throw the object keys and check if there is any object there */
+        keys.forEach(key => {
+            if (typeof obj[key] !== 'object') {
+                // Generate the heirarchy
+                parent ? res.push(`${parent}.${key}`) : res.push(key);
+            } else {
+                // If object found then recursively call the function with updpated parent
+                let newParent = parent ? `${parent}.${key}` : key;
+                getKey(obj[key], res, newParent);
+            }
+
+        });
     }
+
+    function printAllVals(obj, getAttributeValue) {
+        for (let k in obj) {
+            if (typeof obj[k] === "object") {
+                printAllVals(obj[k])
+            } else {
+                // base case, stop recurring
+                if (k === getAttributeValue) {
+                    console.log(obj[k]);
+                }
+            }
+        }
+    }
+
+    //printAllVals(configurationsObj);
+
+    function getValue(objMap, envName, getAttributeValue) {
+        const keys = Object.keys(objMap);
+        keys.forEach(key => {
+            if (key === envName) {
+                // console.log(key);
+                // console.log(objMap[key]);
+                printAllVals(objMap[key], getAttributeValue);
+            }
+        });
+    }
+
+    getValue(configurationsObj, "staging2", "url");
+    getValue(configurationsObj, "canary2", "userName");
+
+    console.log('****** With Recurssion - End******');
 
 } catch (e) {
     console.log(e);
