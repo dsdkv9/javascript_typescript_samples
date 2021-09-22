813ec960729ece40c55872de4973f65d4a6d2eda
Deepak verma <dsdkv9@gmail.com>
2021-09-22 19:28:59 +0530

    Added java samples

---
+++ java_samples/PrimitiveTypesExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class PrimitiveTypesExample {
+    public static void main(String[] args) {
+        List<String> studentList = new ArrayList<>();
+        studentList.add("S001");
+        studentList.add("S002");
+        System.out.println(studentList);
+    }
+
+}
